ffe436811cf952890dda4942691a8c1627e3d785
LuigiRac <lui.rac06@gmail.com>
2024-12-12 18:21:52 +0100

    main_end

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -7,14 +7,17 @@ function MainContent() {
 
     return (
         <main>
-            <div className="card" style={{ width: '18rem' }}>
-                <img className="card-img-top" src={image} alt="Card image cap"></img>
-                <div className="card-body">
-                    <h5 className="card-title">Titolo del post</h5>
-                    <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, molestiae dolor aspernatur neque hic omnis alias rem perspiciatis sapiente numquam.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+            <div className="container d-flex justify-content-center">
+                <div className="card" style={{ width: '18rem' }}>
+                    <img className="card-img-top" src={image} alt="Card image cap"></img>
+                    <div className="card-body">
+                        <h5 className="card-title">Titolo del post</h5>
+                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, molestiae dolor aspernatur neque hic omnis alias rem perspiciatis sapiente numquam.</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
                 </div>
             </div>
+
         </main>
     );
 };
