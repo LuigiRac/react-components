ddbde556de73c47c1b7206a8a900f229ee6eaa4e
LuigiRac <lui.rac06@gmail.com>
2024-12-12 17:48:17 +0100

    fix img

--- src/components/MainComponents.jsx
+++ src/components/MainComponents.jsx
@@ -3,15 +3,16 @@
 
 function MainComponents() {
 
+    const image = "/img/polmoni.png"
 
     return (
         <main>
             <div className="card" style={{ width: '18rem' }}>
-                {/* <img class="card-img-top" src="/img/polmoni.png" alt="Card image cap"> */}
+                <img className="card-img-top" src={image} alt="Card image cap"></img>
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 className="card-title">Titolo del post</h5>
+                    <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, molestiae dolor aspernatur neque hic omnis alias rem perspiciatis sapiente numquam.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
         </main>
