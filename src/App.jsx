b5c4f6503ca28a9a1c57a4599d9ce1c2ca88e840
LuigiRac <lui.rac06@gmail.com>
2024-12-12 14:43:39 +0100

    create_main; public/img

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './index.css'
 import HeaderComponents from "./components/HeaderComponents.jsx";
+import MainComponents from "./components/MainComponents.jsx";
+
 
 function App() {
 
   return (
     <>
       <HeaderComponents />
+      <MainComponents />
     </>
   )
 }
---
+++ src/components/MainComponents.jsx
@@ -0,0 +1,23 @@
+
+
+
+function MainComponents() {
+
+
+    return (
+        <main>
+            <div className="card" style={{ width: '18rem' }}>
+                {/* <img class="card-img-top" src="/img/polmoni.png" alt="Card image cap"> */}
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+
+
+export default MainComponents
