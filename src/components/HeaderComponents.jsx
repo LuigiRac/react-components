c859d0794682cf7cd5d0064ccd1e5bd7091e3c9c
LuigiRac <lui.rac06@gmail.com>
2024-12-12 14:08:34 +0100

    Header

---
+++ src/components/HeaderComponents.jsx
@@ -0,0 +1,13 @@
+function HeaderComponents() {
+    const title = "Il mio blog";
+
+    return (
+        <header>
+            <div className="title">{title}</div>
+        </header >
+    )
+};
+
+
+
+export default HeaderComponents
